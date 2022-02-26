cda85f0292002f627b067312a9e12ec4a236e8ad
Clyde McQueen <clyde@mcqueen.net>
2022-02-26 08:08:05 -0800

    Include tf2_geometry_msgs.hpp

--- ros/include/Node.hpp
+++ ros/include/Node.hpp
@@ -47,7 +47,7 @@
 
 #include "tf2/convert.h"
 #include "tf2/LinearMath/Transform.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include "tf2_ros/buffer.h"
 #include "tf2_ros/message_filter.h"
 #include "tf2_ros/transform_broadcaster.h"
